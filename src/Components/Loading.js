551cbecd3eaf91f3a5aab43fe75f53992ca30391
Youssef Isaac <youssefisaac002@gmail.com>
2024-08-14 20:48:42 +0300

    minor fixes

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Loading(props) {
   return (
     <>
+    <div style={{display:"none"}}>
     {props.component}
+    </div>
     <div style={{display:'flex', justifyContent:"center", alignItems:"center", minHeight:'99vh', flexDirection:"column", gap:"20px"}}>
     <div id='loading'/>
     <h2>Loading...</h2>
