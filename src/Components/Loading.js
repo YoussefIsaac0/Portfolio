7b6708cbfa23cdd9894a58e77f9607ea13e2d60a
Youssef Isaac <youssefisaac002@gmail.com>
2024-08-14 20:46:05 +0300

    minor fixes

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Loading() {
+export default function Loading(props) {
   return (
     <>
-    <IntroductionSection loading={handleLoading} style={{display:'none'}}/>
+    {props.component}
     <div style={{display:'flex', justifyContent:"center", alignItems:"center", minHeight:'99vh', flexDirection:"column", gap:"20px"}}>
     <div id='loading'/>
     <h2>Loading...</h2>
